26f8acbe639dcb96d32728d5d8a17fa66f9ff2c3
subhranshudash13-dotcom <subhranshu.dash13@gmail.com>
2026-01-08 23:42:23 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@
 export function formatPrice(price: number): string {
     return new Intl.NumberFormat('en-US', {
         style: 'currency',
-        currency: 'USD',
+        currency: 'INR',
     }).format(price);
 }
 // Format date
